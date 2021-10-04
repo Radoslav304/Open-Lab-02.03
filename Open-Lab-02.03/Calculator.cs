9d91a2f75a9e4bbd07571bdcc9998d235c9a575c
Radoslav304 <radoslavcabadaj304@gmail.com>
2021-10-04 18:03:29 +0200

    A function that returns the remainder of two numbers.

--- Open-Lab-02.03/Calculator.cs
+++ Open-Lab-02.03/Calculator.cs
@@ -6,7 +6,9 @@ namespace Open_Lab_02._03
     {
         public int Remainder(int dividend, int divisor)
         {
-            throw new NotImplementedException();
+            int quotient;
+            quotient = dividend % divisor;
+            return quotient;
         }
     }
 }
